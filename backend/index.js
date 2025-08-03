86c7f587850bb476b55a01ce57e2b7664653e925
veerbhan <veerbhansingh70@gmail.com>
2025-08-03 10:09:04 +0530

    Handle cors error

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import categoryRoutes from './routes/category.route.js';
 import productRoutes from './routes/product.route.js';
+import cors from 'cors'
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/categories', categoryRoutes);
 app.use('/api/products', productRoutes)
